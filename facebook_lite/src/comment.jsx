99a4651754900de020a4d9ecac0b5944521eecf3
Cyril Pierron <cype59@gmail.com>
2019-11-12 10:19:50 +0100

    fonction c'est super

--- facebook_lite/src/comment.jsx
+++ facebook_lite/src/comment.jsx
@@ -6,6 +6,23 @@ import './index.css';
 
 
 class Comment extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handleSuper = this.handleSuper.bind(this);
+
+    this.state = {
+      nbSuper: 0
+    }
+  }
+
+  handleSuper(event) {
+    event.preventDefault();
+    this.setState({nbSuper: this.state.nbSuper + 1});
+  }
+
+
     render() {
       return  (
         <div class="profil">
@@ -16,7 +33,8 @@ class Comment extends React.Component {
                 </div>
             </div>
             <div class="bouttonSuper">
-                    <Button variant="light">C'est super</Button>
+                    <Button variant="light" onClick={this.handleSuper}>C'est super</Button>
+                    <p>Nombre : {this.state.nbSuper}</p>
             </div>
         </Card>
         </div>
